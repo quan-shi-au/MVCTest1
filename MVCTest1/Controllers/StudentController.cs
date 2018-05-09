067f48d746bbf1f01a9bff71f7c795afa86a1822
DEV-SAM\Sam Quan <sam.quan@wontok.com>
2018-05-10 08:57:37 +1000

    no message

---
+++ MVCTest1/Controllers/StudentController.cs
@@ -0,0 +1,72 @@
+﻿using MVCTest1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCTest1.Controllers
+{
+    public class StudentController : Controller
+    {
+        static List<Student> studentsList = new List<Student>();
+
+        static StudentController()
+        {
+            studentsList.Add
+            (
+                new Student
+                {
+                    FirstName = "Dummy",
+                    LastName = "Dummy",
+                    DOB = DateTime.Now.AddYears(-18),
+                    FathersName = "Dummy",
+                    MothersName = "Dummy"
+                }
+            );
+        }
+
+        //
+        // GET: /Student/
+
+        public ActionResult Index()
+        {
+            return View(studentsList);
+        }
+
+        //
+        // GET: /Student/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Student/Create
+
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                Student student = new Student();
+
+                student.FirstName = collection["FirstName"];
+                student.LastName = collection["LastName"];
+                DateTime suppliedDate;
+                DateTime.TryParse(collection["DOB"], out suppliedDate);
+                student.DOB = suppliedDate;
+                student.FathersName = collection["FathersName"];
+                student.MothersName = collection["MothersName"];
+
+                studentsList.Add(student);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ MVCTest1/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCTest1.Models
+{
+    public class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DOB { get; set; }
+        public string FathersName { get; set; }
+        public string MothersName { get; set; }
+    }
+}
