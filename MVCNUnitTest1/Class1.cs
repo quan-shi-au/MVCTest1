5aac3ce8d0fdf2b868f5ecc6e3204ed95d049264
DEV-SAM\Sam Quan <sam.quan@wontok.com>
2018-05-21 11:55:32 +1000

    no message

---
+++ MVCNUnitTest1/Class1.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVCNUnitTest1
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void FirstTest()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
